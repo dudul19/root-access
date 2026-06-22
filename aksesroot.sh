4deccf610c2b1cd46748271dcd0e7636f19035fd
Pak Dudul <78849151+dudul19@users.noreply.github.com>
2026-06-22 22:55:20 +0700

    Update aksesroot.sh

--- aksesroot.sh
+++ aksesroot.sh
@@ -1,20 +1,30 @@
 #!/bin/bash
-# xixi
-# echo "$crot    ALL=(ALL:ALL) ALL" >> /etc/sudoers;
-wget -q -O /tmp/sshd_config https://raw.githubusercontent.com/hokagelegend9999/alpha.v2/refs/heads/main/sshd_config && sudo mv /tmp/sshd_config /etc/ssh/sshd_config;
-systemctl restart ssh;
-clear;
-echo -e "Masukkan Password:";
-read -e pwe;
+
+if [ "$(id -u)" != "0" ]; then
+   echo "This script must be run as root! Type 'sudo su' first." 1>&2
+   exit 1
+fi
+
+clear
+wget -q -O /tmp/sshd_config https://raw.githubusercontent.com/dudul19/root-access/main/sshd_config && sudo mv /tmp/sshd_config /etc/ssh/sshd_config
+systemctl restart ssh
+
+clear
+echo "======================================"
+echo " ❖ Create Root Access"
+echo "--------------------------------------"
+read -p " New Password : " pwe
+
 usermod -p "$(perl -e "print crypt('$pwe', 'Q4')")" root;
-clear;
-printf "Mohon Simpan Informasi Akun VPS Ini
-============================================
-Akun Root (Akun Utama)
-Ip address = $(curl -Ls http://ipinfo.io/ip)
-Username   = root
-Password   = $pwe
-============================================
-"
-echo "";
-exit;
+
+clear
+echo "======================================"
+echo " ❖ Detail VPS"
+echo "--------------------------------------"
+echo " IP Address : $(curl -Ls http://ipinfo.io/ip)"
+echo " Username   : root"
+echo " Password   : $pwe"
+echo "======================================"
+echo
+read -n 1 -s -r -p "Press any key to exit"
+exit
